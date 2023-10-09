c03511cd67edd7b81a86f8a56b8320017fc38d12
ilham <ilhammunawar55@gmail.com>
2023-10-09 10:34:41 +0700

    Create variables.php

---
+++ jobstheet1/variables.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables PHP</title>
+</head>
+<body>
+    <?php
+    echo "Contoh 1 </br>";
+    $integer=25;
+    $string="Praktikum Web II";
+    $boolean=true;
+
+    echo "Ini Contoh Integer $integer </br>";
+    echo "Ini contoh String $string </br>";
+    echo "Ini contoh Boolean $boolean </br>";
+
+    $integer = 50;
+    $string = "Belajar PHP";
+    $boolean = false;
+
+    echo "Contoh 2: </br>";
+    echo "Ini Contoh Integer: $integer </br>";
+    echo "Ini contoh String: $string </br>";
+    echo "Ini contoh Boolean: $boolean </br>";
+    ?>
+
+</body>
+</html>
